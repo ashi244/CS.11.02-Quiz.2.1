901ad50fe32391f5f33281403d5c3a85074ac541
angelashi <angelas2026@student.cis.edu.hk>
2023-09-12 14:10:59 +0800

    Finished Quiz 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int resultOne = add(12, 13);
+        System.out.println(resultOne);
+        String resultTwo = greeting("Lewis");
+        System.out.println(resultTwo);
+        int resultThree = add(4, 5, 6, 7);
+        System.out.println(resultThree);
+        printMe("Yay!");
+    }
+    public static int add(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
+    public static String greeting(String name){
+        return "Bonjour, " + name + "!";
+    }
+    public static int add(int numOne, int numTwo, int numThree, int numFour){
+        return add(add(add(numOne, numTwo), numThree), numFour);
+    }
+    public static void printMe(String toBePrinted){
+        System.out.println(toBePrinted);
+    }
 }
